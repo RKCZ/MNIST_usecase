4d20bc149e08312697392a2cd3b2b057be747ebe
Roman Kalivoda <kalivodaroman@gmail.com>
2020-04-12 19:25:45 +0200

    Update train_keras.py
    Refactoring

--- train_keras.py
+++ train_keras.py
@@ -28,47 +28,28 @@ def train():
     batch_size = 1024
     num_classes = 10
     epochs = 12
+    dataset_path = 'mnist.h5'
 
     # create dirs for result files
     path_wd = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(
         __file__)), 'temp', str(time.time())))
     os.makedirs(path_wd)
-
-    dataset_path = 'mnist.h5'
     # load dataset
     dataset = mnist.load_data(path=dataset_path)
     (x_train, y_train), (x_test, y_test) = dataset
-
+    
     # preprocess data
     x_train = x_train / 255
     x_test = x_test / 255
-
     axis = 1 if keras.backend.image_data_format() == 'channels_first' else -1
-
     x_train = np.expand_dims(x_train, axis)
     x_test = np.expand_dims(x_test, axis)
-
     y_train = keras.utils.np_utils.to_categorical(y_train)
     y_test = keras.utils.np_utils.to_categorical(y_test)
     input_shape = x_train.shape[1:]
-
-    # model = Sequential()
-    # model.add(Conv2D(
-    #     32,
-    #     kernel_size=(3, 3),
-    #     activation=keras.activations.relu,
-    #     input_shape=input_shape
-    #     ))
-    # model.add(Conv2D(64, kernel_size=(3, 3),
-    #                  activation=keras.activations.relu))
-    # model.add(Dropout(0.25))
-    # model.add(Flatten())
-    # model.add(Dense(128, activation=keras.activations.relu))
-    # model.add(Dropout(0.5))
-    # model.add(Dense(num_classes))
-
+    
+    # create the ANN model
     input_layer = Input(input_shape)
-
     layer = Conv2D(filters=16,
                kernel_size=(5, 5),
                strides=(2, 2))(input_layer)
@@ -90,16 +71,14 @@ def train():
     layer = Dropout(0.01)(layer)
     layer = Dense(units=10,
               activation='softmax')(layer)
-
     model = Model(input_layer, layer)
     model.summary()
 
+    # train and test the model
     loss_fn = 'categorical_crossentropy'
-
     model.compile(optimizer='adam',
                   loss=loss_fn,
                   metrics=['accuracy'])
-
     model.fit(
         x_train,
         y_train,
@@ -113,16 +92,15 @@ def train():
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
 
-    # save trained model
+    # save trained model and test data
     model_name = 'keras_model'
     keras.models.save_model(
         model,
         os.path.join(path_wd, model_name + '.h5')
         )
-
     np.savez_compressed(os.path.join(path_wd, 'x_test'), x_test)
     np.savez_compressed(os.path.join(path_wd, 'y_test'), y_test)
     np.savez_compressed(os.path.join(path_wd, 'x_norm'), x_train[::10])
-
     print('Model and dataset saved to {}'.format(path_wd))
+    
     return path_wd
